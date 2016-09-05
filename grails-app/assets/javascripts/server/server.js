e3a1c31cf9398be45e1e1a4bec6486eb1f722b61
sardylan <sardylan@gmail.com>
2016-09-05 14:13:56 +0200

    Initial commit

---
+++ grails-app/assets/javascripts/server/server.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /server/core/server.core
+//= require /server/index/server.index
+
+angular.module("server", [
+    "server.core",
+    "server.index"
+]);
